056f45b63b2803e56b3091d2aaea6687d7855da4
georgina lima del carmen <lageorgi_lima_@hotmail.com>
2018-11-28 20:33:03 -0400

    creacion de los modelos

---
+++ Final/Final/Models/factura .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Models
+{
+    public class factura
+    {
+        public int id{ get; set; }
+        public int total  { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+        public productos productos { get; set; }
+        public int productosid { get; set; }
+    }
+}
---
+++ Final/Final/Models/productos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Models
+{
+    public class productos
+    {
+        public int id { get; set; }
+
+        public string nombre { get; set; }
+        public string detalle { get; set; }
+        public int cantidad  { get; set; }
+        public int precios  { get; set; }
+    }
+}
