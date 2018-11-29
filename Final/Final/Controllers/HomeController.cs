eb159256c10aad7f06993f218ddaf2de5863a52d
Mario David Pichardo Vasquez <mariopv90@gmail.com>
2018-11-28 21:09:39 -0400

    ajustando registros y otros cambios

--- Final/Final/Controllers/HomeController.cs
+++ Final/Final/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Final.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Final.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
