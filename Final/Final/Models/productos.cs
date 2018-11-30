a22f7e31c774b33e4d8b79cf114f4dc04b71487f
Mario David Pichardo Vasquez <mariopv90@gmail.com>
2018-11-29 22:42:31 -0400

    cambios a modelos de factura y productos y parte del login

--- Final/Final/Models/productos.cs
+++ Final/Final/Models/productos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,13 @@ namespace Final.Models
     public class productos
     {
         public int id { get; set; }
-
+        [Required]
         public string nombre { get; set; }
+        [Required]
         public string detalle { get; set; }
+        [Required]
         public int cantidad  { get; set; }
+        [Required]
         public int precios  { get; set; }
     }
 }
